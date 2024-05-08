535226271c24844c5e1f575ba8c303f3d33dd066
Pablo Nieto Arias <104018391+PabloNietoA@users.noreply.github.com>
2024-05-08 09:37:18 +0200

    Update con modificadores de listas

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,35 @@
 object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
+  //dimensiones de la matriz
+  val altura: Int = pedirValor("altura")
+  val anchura: Int = pedirValor("anchura")
+
+  def generarTablero(n:Int):List[Char] = n match
+  {
+    case 0 => Nil
+    case num => '.'::generarTablero(num-1)
+  }
+
+  def listaVacia(l:List[Char]): Boolean = l match
+  {
+    case Nil => true
+    case _ :: _ => false
+  }
+
+  def longitud(l:List[Char]): Int = l match
+  {
+    case Nil => 0
+    case _ :: r => 1 + longitud(r)
+  }
+
+  def subLista (i : Int, f : Int, l : List[Char]) : List[Char] = l match
+  {
+    case Nil => Nil
+    case _ if (longitud(l) < f) => throw new Error("Longitud de lista excedida")
+    case _ if (f < 0 || i < 0) => throw new Error("Ni el inicio ni final pueden ser negativos")
+    case _ if (f > i) => throw new Error("El inicio debe ser menor que el final")
+
+    case _ :: r if (i > 0) => subLista(i-1, f-1, r)
+    case p :: r if (i == 0 && f > 0) => p :: subLista(i, f-1, r)
+    case _ :: _ if (i == 0 && f == 0) => Nil
   }
 }
---
+++ src/Paquete.scala
@@ -0,0 +1,9 @@
+package ops {
+  class Operaciones {
+    def suma(a: Int, b: Int): Int = {
+      return a + b
+    }
+
+    def resta(a: Int, b: Int): Int = a - b
+  }
+}
