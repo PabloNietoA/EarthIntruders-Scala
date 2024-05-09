85b5b9892cd9eeba3062c89b37a9ef4162139869
Pablo Nieto Arias <104018391+PabloNietoA@users.noreply.github.com>
2024-05-09 14:54:09 +0200

    Update

--- src/Main.scala
+++ src/Main.scala
@@ -1,29 +1,23 @@
-import scala.util.Random
+import scala.util.Random, scala.math.ceil
 
 object Main
 {
   //dimensiones de la matriz
-  val altura: Int = 10
+  val altura: Int = 7
   val anchura: Int = 15
 
-  def generarTablero(n:Int):List[Char] = n match
-  {
-    case 0 => Nil
-    case num => ' '::generarTablero(num-1)
-  }
+
 
   def listaVacia(l:List[Char]): Boolean = l match
   {
     case Nil => true
     case _ :: _ => false
   }
-
   def longitud(l:List[Char]): Int = l match
   {
     case Nil => 0
     case _ :: r => 1 + longitud(r)
   }
-
   def sublist (i : Int, f : Int, l : List[Char]) : List[Char] = l match
   {
     case Nil => Nil
@@ -35,7 +29,6 @@ object Main
     case p :: r if (i == 0 && f > 0) => p :: sublist(i, f-1, r)
     case _ :: _ if (i == 0 && f == 0) => Nil
   }
-
   def concat (l1:List[Char], l2:List[Char]): List[Char] = l1 match
   {
     case Nil => l2 match
@@ -45,21 +38,18 @@ object Main
     }
       case p :: r => p::concat(r, l2)
   }
-
   def reverse (l:List[Char]):List[Char] = l match
   {
     case Nil => Nil
     case p :: r => concat(reverse(r), p::Nil)
     case x => x
   }
-
   def get(i:Int, l:List[Char]):Char = l match {
     case Nil => throw new Error("Index out of range")
     case p :: _ if (i == 0) => p
     case _ :: r if (i > 0) => get(i-1, r)
     case _ => throw new Error("Negative index")
   }
-
   def replace(c:Char, i:Int, l:List[Char]):List[Char] = {
     if (i >= longitud(l) || i < 0) throw new Error("Index out of range")
     val lizq = sublist(0, i, l)
@@ -67,14 +57,45 @@ object Main
     return concat(lizq, c::lder)
   }
 
+  def generarTablero(n:Int):List[Char] = n match
+  {
+    case 0 => Nil
+    case num => ' '::generarTablero(num-1)
+  }
   def printTablero(t:List[Char]):Unit = {
     t match {
       case Nil => print("\n")
       case p :: r if (longitud(r) % anchura == 0) =>
-        print("["+p+"]\n")
+        if (p == 'A')
+          print("["+Console.GREEN+p+Console.RESET+"]\n")
+        else if (p == 'N')
+          print("["+"\u001B[38;5;130m"+p+Console.RESET+"]\n")
+        else if (p == 'C')
+          print("["+Console.BLUE+p+Console.RESET+"]\n")
+        else if (p == 'D')
+          print("["+"\u001B[38;5;169m"+p+Console.RESET+"]\n")
+        else if (p == 'R')
+          print("["+Console.YELLOW+p+Console.RESET+"]\n")
+        else if (p == 'X')
+          print("["+Console.RED+p+Console.RESET+"]\n")
+        else
+          print("["+p+"]\n")
         printTablero(r)
       case p :: r =>
-        print("["+p+"]")
+        if (p == 'A')
+          print("["+Console.GREEN+p+Console.RESET+"]")
+        else if (p == 'N')
+          print("["+"\u001B[38;5;130m"+p+Console.RESET+"]")
+        else if (p == 'C')
+          print("["+Console.BLUE+p+Console.RESET+"]")
+        else if (p == 'D')
+          print("["+"\u001B[38;5;169m"+p+Console.RESET+"]")
+        else if (p == 'R')
+          print("["+Console.YELLOW+p+Console.RESET+"]")
+        else if (p == 'X')
+          print("["+Console.RED+p+Console.RESET+"]")
+        else
+          print("["+p+"]")
         printTablero(r)
     }
   }
@@ -93,7 +114,6 @@ object Main
     }
     else return Nil
   }
-
   def bajarAliens(t:List[Char]):List[Char] = {
     val avance = sublist(0, longitud(t)-anchura, t)
     val add = generarAliens(anchura)
@@ -154,13 +174,11 @@ object Main
       return borrarTransf(i-1, l)
     }
   }
-
   def marcarTransfs(l:List[Char]):List[Char] = marcarTransfsAux(anchura*altura, l)
   def marcarTransfsAux(i:Int, l:List[Char]):List[Char] = {
     if (i == 0) return l
     else return marcarTransfsAux(i-1, marcarTransf(i, l))
   }
-
   def marcarTransf(i:Int, t:List[Char]):List[Char] = {
     if (i > anchura && i < longitud(t)-anchura && i % anchura > 0 && i % anchura < anchura-1)
     {
@@ -188,14 +206,141 @@ object Main
     return t
   }
 
+  def generarPlayer(n:Int):List[Char] = n match {
+    case 0 => Nil
+    case i if (i-1 == anchura/2) => 'W'::generarPlayer(n-1)
+    case _ => ' '::generarPlayer(n-1)
+  }
+  def getPosPlayer(l:List[Char]):Int = anchura - getPosPlayerAux(anchura, l)
+  def getPosPlayerAux(i:Int, l:List[Char]):Int = l match {
+    case Nil => throw new Error("No hay W")
+    case 'W'::_ => i
+    case _::r => getPosPlayerAux(i-1, r)
+  }
+  def moverPlayer(p:List[Char]):List[Char] = scala.io.StdIn.readLine().toLowerCase() match {
+    case "a" =>
+      val player = getPosPlayer(p)
+      return replace('W',player-1,replace(' ', player, p))
+    case "d" =>
+      val player = getPosPlayer(p)
+      return replace('W',player+1,replace(' ', player, p))
+    case _ => return p
+  }
+  def dibujarPlayer(t:List[Char], p:List[Char]):List[Char] = {
+    val posPRel = getPosPlayer(p)
+    val iniPRow = longitud(t)-anchura
+    val posP = posPRel + iniPRow
+    return replace('W', posP, t)
+  }
+
+  def perderVida(p:List[Char], t:List[Char], v:Int):Int = {
+    val posPRel = getPosPlayer(p)
+    val iniPRow = longitud(t)-anchura
+    val posP = posPRel + iniPRow
+    if (get(posP, t) != ' ') return v-1
+    return v
+  }
+
+  def generarDefensas(n:Int, contador:Int):List[Char] = n match
+  {
+    case 0 => Nil
+    case _ if (contador >= 3) => ' '::generarDefensas(n-1, 0)
+    case _ if (new Random().nextInt() % 101 <= 15) => 'B'::generarDefensas(n-1, contador+1)
+    case _ => ' '::generarDefensas(n-1,0)
+  }
+  def dibujarDefensas(t:List[Char], m:List[Char], i:Int):List[Char] = {
+    val iniPRow = longitud(t)-anchura*5
+    val pos = i + iniPRow
+    if (i == anchura) return t
+    else if (get(i, m) == 'B') return dibujarDefensas(replace('B', pos, t), m, i+1)
+    else return dibujarDefensas(t, m, i+1)
+  }
+  def borrarDefensas(t:List[Char], i:Int):List[Char] = {
+    val iniPRow = longitud(t)-anchura*4
+    val pos = i + iniPRow
+    if (i == anchura) return t
+    else if (get(pos, t) == 'B') return borrarDefensas(replace(' ', pos, t), i+1)
+    else return borrarDefensas(t, i+1)
+  }
+  def romperDefensas(t:List[Char], m:List[Char], i:Int):List[List[Char]] = {
+    val iniPRow = longitud(t)-anchura*5
+    val pos = i + iniPRow
+    if (i < anchura && get(pos, t) == 'R' && get(i, m) == 'B')
+      {
+        if (new Random().nextInt() % 101 <= 50)
+        {
+          val tablero = borrarColumna(i, t)
+          val muros = replace(' ', i, m)
+          return romperDefensas(tablero, muros, i+1)
+        }
+        else
+        {
+          val tableroymuros = borrarFila(pos, t, m)
+          val tablero = getList(0, tableroymuros)
+          val muros = getList(1, tableroymuros)
+          return List(tablero, muros)
+        }
+      }
+    else if (i < anchura) return romperDefensas(t, m, i+1)
+    else return List(t, m)
+  }
+
+  def borrarFila(i:Int, t:List[Char], m:List[Char]): List[List[Char]] =
+  {
+    val fila = i / anchura
+    print(fila + " " + (longitud(t) / anchura - 4))
+    if (fila == longitud(t) / anchura - 5) return List(borrarFilaAux(fila,t), generarTablero(anchura))
+    else return List(borrarFilaAux(fila,t), m)
+
+  }
+  def borrarFilaAux(i:Int, t:List[Char]):List[Char] =
+  {
+    val filaIni = i*anchura
+    val principio = if (filaIni == 0) Nil else sublist(0, filaIni, t)
+    val fin = if (filaIni == longitud(t)-anchura) Nil else sublist(filaIni+anchura, longitud(t), t)
+    return concat(principio, concat(generarTablero(anchura), fin))
+  }
+
+  def borrarColumna(i:Int, t:List[Char]):List[Char] = borrarColumnaAux(0,i,t)
+  def borrarColumnaAux(n:Int, i:Int, tablero:List[Char]): List[Char] ={
+    val pos = n*anchura + i
+    print(n + " " + pos + " " + i + "\n")
+    if(n<altura && get(pos, tablero) == 'W') return borrarColumnaAux(n+1,i,replace('w', pos, tablero))
+    else if(n<altura) return borrarColumnaAux(n+1,i,replace(' ', pos, tablero))
+    else return tablero
+  }
+
+  def getList(i:Int, l:List[List[Char]]):List[Char] = l match {
+    case Nil => throw new Error("Index out of range")
+    case p :: _ if (i == 0) => p
+    case _ :: r if (i > 0) => getList(i-1, r)
+    case _ => throw new Error("Negative index")
+  }
+
+  def jugar(p:List[Char], t:List[Char], m:List[Char], v:Int):Unit =
+  {
+    if(v > 0)
+    {
+      val tableroAux = transformar(bajarAliens(t))
+      val tableroymuros = romperDefensas(tableroAux, m, 0)
+      val muros = getList(1, tableroymuros)
+      val tableroAux2 = dibujarDefensas(borrarDefensas(getList(0, tableroymuros), 0), muros, 0)
+      val vida = perderVida(p, tableroAux2, v)
+      print("Vidas: "+vida+"\n")
+      val tablero = dibujarPlayer(tableroAux2, p)
+      printTablero(tablero)
+      val player = moverPlayer(p)
+      jugar(player, tablero, muros, vida)
+    }
+    if (v == 0) print("\nGAME OVER\n")
+  }
+
   def main (args:Array[String]):Unit =
   {
-    val l1 = List('n','b', 'c')
-    val l2 = List('c', 'd')
+    val muros = generarDefensas(anchura, 0)
+    val player = generarPlayer(anchura)
     val t = generarTablero(altura*anchura)
-    print("\n")
-    val x = bajarAliens(bajarAliens(bajarAliens(bajarAliens(bajarAliens(t)))))
-    printTablero(transformar(x))
+    jugar(player, t, muros, 3)
   }
 
 }
--- src/li/Interfaz.scala
+++ src/li/Interfaz.scala
@@ -1,8 +1,10 @@
+import scala.math.ceil
+
 package li
 {
   class Interfaz (l:List[Char])
   {
-
+    def x(i:Double):Double = ceil(i)
 
 
   }
